fc2d9ba021e4ab6783db90b1c89c6bde50d833ef
Amanda Gomes <amanda@MacBook-Pro.local>
2022-05-24 20:12:29 -0300

    [amanda] Criação do site

---
+++ application/views/cursos.php
@@ -0,0 +1,28 @@
+<div class="site-content-inner">
+  <section class="site-content-section">
+    <div class="site-content-section-inner section-two-columns">
+      <div class="curso-banner-text">
+        <h1>Cursos</h1> 
+        <div>
+          <h2>Participe do nosso treinamento para barbeiros online.</h2>
+          <p>No nosso curso Online você tem acesso a diversas aulas com profissionais altamente qualificados, aprende o que há de mais moderno no mercado e ainda pode conquista seu certificado!<p>
+          <ul>
+            <li>Conteúdo exclusivo.</li>
+            <li>Material de apoio.</li>
+            <li>Aulas disponíveis em qualquer horário.</li>
+          </ul>
+        </div>
+        <div class="curso-banner-button">
+          <a href="" target="_blank" class="button-link" role="button">
+            <span class="button-link-wrapper">
+              <span class="button-link-text">Inscreva-se</span>
+            </span>
+          </a>
+        </div>
+      </div>
+      <div>
+        <img src="<?php echo base_url(); ?>public/images/cursos/curso_banner.jpeg" alt="">
+      </div>
+    </div> 
+  </section> 
+</div>
---
+++ application/views/sobre.php
@@ -0,0 +1,20 @@
+<div class="site-content-inner">
+  <section class="site-content-section">
+    <div class="site-content-section-inner section-two-columns">
+      <div class="curso-banner-text">
+        <h1>Sobre</h1> 
+        <div>
+          <p>Fundada em 11 de fevereiro de 2011 pelos amigos William Miller e Federico Castro, a barbearia Uncle Sam's conta com tudo que o homem moderno precisa. Um ambiente confortável, descontraído e exclusivamente masculino, com cuidados para todos os estilos de barba e cabelo, além de outros serviços.<p>
+          <p>A Uncle Sam Barbershop  é um espaço sagrado para o homem, onde ele pode relaxar, tomar um bom drink, conversar, encontrar amigos, fazer negócios, viver a experiência de um gentleman. Porque nós acreditamos que assim deve ser uma Barbearia.</p>
+        </div>
+      </div>
+      <div>
+        <img src="<?php echo base_url(); ?>public/images/sobre/barbearia.webp" alt="">
+      </div>
+    </div> 
+  </section>
+  <section class="site-content-section section-margin section-background-image">
+    <div class="site-content-section-inner">
+    </div> 
+  </section>
+</div>
